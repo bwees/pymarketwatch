34ec8b80cde9b947303aaef762c60e6279dff9a6
bwees <brandonwees@gmail.com>
2021-11-03 11:27:22 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,26 +9,20 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="realpython-reader",
+    name="pymarketwatch",
     version="1.0.0",
-    description="Read the latest Real Python tutorials",
+    description="API to interact with MarketWatch's Stock Market Game API",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/realpython/reader",
-    author="Real Python",
-    author_email="info@realpython.com",
-    license="MIT",
+    url="https://github.com/bwees/pymarketwatch",
+    author="bwees",
+    author_email="brandonwees@gmail.com",
+    license="Apache 2.0",
     classifiers=[
-        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["reader"],
+    packages=["pymarketwatch"],
     include_package_data=True,
-    install_requires=["feedparser", "html2text"],
-    entry_points={
-        "console_scripts": [
-            "realpython=reader.__main__:main",
-        ]
-    },
+    install_requires=["lxml", "requests", "urllib"]
 )
